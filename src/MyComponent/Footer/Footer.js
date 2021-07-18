6a1660a45a13483e54e567496fd3899c98c5239b
RvTechiNNovate <vritesh066@gmail.com>
2021-07-18 19:51:48 +0530

    first commit

---
+++ src/MyComponent/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+export const Footer = () => {
+    return (
+        
+            <div className="footer">
+                <p>Copyright &copy; 2021 All right reserved </p>
+            </div>
+       
+    )
+}
---
+++ src/MyComponent/Welcome/Welcome.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useTypewriter, Cursor} from 'react-simple-typewriter'
+
+import "./Welcome.css"
+
+export const Welcome = () => {
+
+  const text = useTypewriter({
+    words: ["Data Science", "Web Development",
+    "Artificial Inteligence","Cyber Security", "Python","Java","C++"],
+    loop:0,
+    typeSpeed:200,
+    delaySpeed:2000,
+    deleteSpeed:50,
+
+  })
+
+  return (
+    <div className='heading-animated'>
+        <p>
+      <span>Welcome to the world of </span><span className="span-typed-text">{text} </span><span><Cursor /> Project</span>
+      </p>
+      
+    </div>
+  )
+}
